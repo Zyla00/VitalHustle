993803d9cf6c26374c42b5f2ae9fc1c2ccd32427
Zyla00 <zyla.angelika2000@gmail.com>
2024-06-18 17:01:12 +0200

    Calendar -> display of past data

--- cald/views.py
+++ cald/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from wellnessTracker.models import Day
 
-class CaldView(LoginRequiredMixin, TemplateView):
+class CaldView(LoginRequiredMixin, ListView):
     template_name = 'calendar.html'
     login_url = '/login/'
     redirect_field_name = 'next'
+    model = Day
+    context_object_name = 'days'
+
+    def get_queryset(self):
+        return Day.objects.filter(user=self.request.user).order_by('-date')
