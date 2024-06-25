c2f98885b38362000dd7bc1df9844e8a984ac787
Zyla00 <zyla.angelika2000@gmail.com>
2024-06-25 18:36:06 +0200

    trying to deploy

--- webApp/settings.py
+++ webApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key-for-lo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['*']
 
 load_dotenv()
 
